43393c511fbbb2df228ad8493cad7468501ba844
mben429 <mtbennett9010@gmail.com>
2021-01-31 16:48:15 +1300

    changes to recentmatches

--- general/static/general/js/RecentMatches.js
+++ general/static/general/js/RecentMatches.js
@@ -1,9 +1,53 @@
 // USe this to alter, and update REcent Matches information 
 
 const recentMatchesInfo = {
-    "massey1stxv": "<text style='color: var(--green-win);'>W</text>vs Rangitoto", 
-    "rosmini1stxv": "var(--rosmini-blue)",
-    "crusadersrugby": "var(--crusaders-red)",
-    "canesrugby": "var(--canes-yellow)",
-    "toulonrugby": "var(--toulon-red)",
+    "massey1stxv": [
+        "<text style='color: var(--green-win);'>W</text>vs Rangitoto",
+        "<text style='color: var(--green-win);'>W</text>vs Rosmini College",
+        "<text style='color: var(--red-lose);'>L</text>vs Westlake",
+        "<text style='color: var(--green-win);'>W</text>vs Takapuna Grammar",
+        "<text style='color: var(--green-win);'>W</text>vs Orewa College"
+    ], 
+    "rosmini1stxv": [
+        "<text style='color: var(--green-win);'>W</text>vs Manurewa HS",
+        "<text style='color: var(--green-win);'>L</text>vs Massey HS",
+        "<text style='color: var(--red-lose);'>L</text>vs Westlake",
+        "<text style='color: var(--green-win);'>W</text>vs Takapuna Grammar",
+        "<text style='color: var(--green-win);'>W</text>vs Orewa College"
+    ],
+    "crusadersrugby": [
+        "<text style='color: var(--green-win);'>W</text>vs Rangitoto",
+        "<text style='color: var(--green-win);'>W</text>vs Rosmini College",
+        "<text style='color: var(--red-lose);'>L</text>vs Westlake",
+        "<text style='color: var(--green-win);'>W</text>vs Takapuna Grammar",
+        "<text style='color: var(--green-win);'>W</text>vs Orewa College"
+    ],
+    "canesrugby": [
+        "<text style='color: var(--green-win);'>W</text>vs Rangitoto",
+        "<text style='color: var(--green-win);'>W</text>vs Rosmini College",
+        "<text style='color: var(--red-lose);'>L</text>vs Westlake",
+        "<text style='color: var(--green-win);'>W</text>vs Takapuna Grammar",
+        "<text style='color: var(--green-win);'>W</text>vs Orewa College"
+    ],
+    "toulonrugby": [
+        "<text style='color: var(--green-win);'>W</text>vs Rangitoto",
+        "<text style='color: var(--green-win);'>W</text>vs Rosmini College",
+        "<text style='color: var(--red-lose);'>L</text>vs Westlake",
+        "<text style='color: var(--green-win);'>W</text>vs Takapuna Grammar",
+        "<text style='color: var(--green-win);'>W</text>vs Orewa College"
+    ],
+}
+
+const recentMatchesScores = {
+    "massey1stxv": [
+        "<text style='color: var(--green-win);'>W</text>vs Rangitoto",
+        "<text style='color: var(--green-win);'>W</text>vs Rosmini College",
+        "<text style='color: var(--red-lose);'>L</text>vs Westlake",
+        "<text style='color: var(--green-win);'>W</text>vs Takapuna Grammar",
+        "<text style='color: var(--green-win);'>W</text>vs Orewa College"
+    ], 
+    "rosmini1stxv": [],
+    "crusadersrugby": [],
+    "canesrugby": [],
+    "toulonrugby": [],
 }
