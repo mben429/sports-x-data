2dbe30a7d642fae469f1b3c33aac503a3cacd971
mben429 <mtbennett9010@gmail.com>
2021-02-23 16:43:49 +1300

    added logout functionality for auth users. some styling fixes as well

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import loginView, loggedinView
+from .views import loginView, loggedinView, logoutView
 
 urlpatterns = [
     path('login/', loginView, name="login"),
-    path('loggedin/', loggedinView, name="loggedin")
+    path('loggedin/', loggedinView, name="loggedin"),
+    path('logout/', logoutView, name="logout"),
 ]
 
 
--- users/views.py
+++ users/views.py
@@ -86,3 +86,50 @@ def loggedinView(request):
     return render(request, "registration/login_success.html", {'data' : dataJSON})
 
     
+def logoutView(request):
+    logout(request)
+    if request.method == "GET":
+        form = LoginForm()
+    else:
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            team_user_name = form.cleaned_data['team_user_name']
+            team_user_key = form.cleaned_data['team_user_key']
+
+            user = authenticate(request, username=team_user_name, password=team_user_key)
+            
+            if user is not None:
+                login(request, user)
+                print("Current team username is: ", request.user.username)
+                
+                # Redirect TeamTestName Login
+                if team_user_name == "TeamTestName":
+                    team_user_name = "rosmini1stxv"
+
+                # Get Data from DB
+                team_table_data = get_team_table_data(team_user_name)
+                team_season_data = get_team_season_table_data(team_user_name)
+                team_game_data = get_team_game_data(team_user_name)
+                team_stat_data = get_team_stat_data(team_user_name)
+                team_game_event_data = get_team_game_event_data(team_user_name)
+                
+                # Dump data into JSON for communication with JS
+                data_dict = {
+                    'team_user_name' : team_user_name,
+                    'organization': "test_obj",
+                    'team_table_data': team_table_data,
+                    'team_season_data': team_season_data,
+                    'team_game_data': team_game_data,
+                    'team_stat_data': team_stat_data,
+                    'team_game_event_data': team_game_event_data
+                }
+                dataJSON = dumps(data_dict, default=str)
+
+                return render(request, "registration/login_success.html", {'data' : dataJSON})
+
+                # Simply testing
+                #return render(request, "registration/test_new_ac.html", {"team_user_name": team_user_name})
+            else:
+                return render(request, "registration/login_failure.html")
+
+    return render(request, "registration/login.html", {'form': form})
