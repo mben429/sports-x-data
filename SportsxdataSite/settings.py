8cd9dcf3275ffa2a39ba31f2b6ac87a67b84b1fd
Michael Bennett <mtbennett9010@gmail.com>
2021-03-10 15:36:56 +1300

    fixing settings config

--- SportsxdataSite/settings.py
+++ SportsxdataSite/settings.py
@@ -18,9 +18,8 @@ import environ
 
 # Initialize environ
 env = environ.Env()
-environ.Env.read_env()
-SECRET_KEY = env('SECRET_KEY')
-
+env.read_env()
+SECRET_KEY = 'SECRET_KEY'
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
