9e216908091248c0601fa182142a8f68533e2b82
Michael Bennett <mtbennett9010@gmail.com>
2021-05-31 20:35:29 +1200

    statics

---
+++ staticfiles/general/js/TeamCentreData.js
@@ -0,0 +1,92 @@
+const tmRef = {
+    "Massey HS": "massey1stxv",
+    "Rosmini College" : "rosmini1stxv",
+    "Crusaders": "crusadersrugby",
+    "Hurricanes": "canesrugby",
+    "Toulon": "toulonrugby",
+}
+
+const tmHeaders = {
+    "massey1stxv" : "Massey BHS 1st XV",
+    "rosmini1stxv" : "Rosmini College 1st XV",
+    "westlake1stxv" : "Westlake BHS 1st XV",
+    "orewa1stxv" : "Orewa College 1st XV",
+    "whangarei1stxv" : "Whangarei BHS 1st XV",
+    "takapuna1stxv" : "Takapuna Grammar 1st XV",
+    "manurewa1stxv" : "Manurewa College 1st XV",
+    "rangitoto1stxv" : "Rangitoto College 1st XV",
+    "mahurangi1stxv" : "Mahurangi College 1st XV"
+}
+
+const tmStdName = {
+    "massey1stxv" : "Massey BHS",
+    "rosmini1stxv" : "Rosmini",
+    "westlake1stxv" : "Westlake BHS",
+    "orewa1stxv" : "Orewa College",
+    "whangarei1stxv" : "Whangarei BHS",
+    "takapuna1stxv" : "Takapuna Grammar",
+    "manurewa1stxv" : "Manurewa College",
+    "rangitoto1stxv" : "Rangitoto",
+    "mahurangi1stxv" : "Mahurangi College"
+}
+
+const lgInfo = {
+    "harbour1stxv_league": [
+        "massey1stxv", 
+        "rosmini1stxv", 
+        "orewa1stxv", 
+        "rangitoto1stxv",
+        "manurewa1stxv", 
+        "whangarei1stxv",
+        "takagrammar1stxv",
+        "mahurangi1stxv",
+        "westlake1stxv",
+    ]
+}
+
+const tmInfo = {
+    "rosmini1stxv": [
+        ["1.", "Rosmini College", "5", "4", "1", "0", "164", "103", "61", "4", "20"],
+        ["2.","Westlake BHS", "5", "3", "2", "0", "140", "103", "37", "4", "16"],
+        ["3.","Massey HS", "5", "3", "2", "0", "136", "88", "48", "4", "16"],
+        ["4.","Whangarei BHS", "5", "3", "2", "0", "138", "91", "47", "3", "15"],
+        ["5.","Rangitoto College", "5", "3", "2", "0", "112", "121", "-9", "3", "15"],
+        ["6.","Takapuna Grammar", "5", "2", "3", "0", "55", "239", "-184", "2", "10"],
+        ["7.","Orewa College", "5", "1", "4", "0", "40", "240", "-200", "1", "5"],
+        ["8.","Manurewa HS", "5", "0", "5", "0", "30", "235", "-205", "1", "1"],
+        ["9.","Mahurangi College", "4", "0", "4", "0", "21", "250", "-229", "0", "0"],
+
+    ],
+
+    "massey1stxv": [
+        ["1.", "Rosmini College", "5", "4", "1", "0", "164", "103", "61", "4", "20"],
+        ["2.","Westlake BHS", "5", "3", "2", "0", "140", "103", "37", "4", "16"],
+        ["3.","Massey HS", "5", "3", "2", "0", "136", "88", "48", "4", "16"],
+        ["4.","Whangarei BHS", "5", "3", "2", "0", "138", "91", "47", "3", "15"],
+        ["5.","Rangitoto College", "5", "3", "2", "0", "112", "121", "-9", "3", "15"],
+        ["6.","Takapuna Grammar", "5", "2", "3", "0", "55", "239", "-184", "2", "10"],
+        ["7.","Orewa College", "5", "1", "4", "0", "40", "240", "-200", "1", "5"],
+        ["8.","Manurewa HS", "5", "0", "5", "0", "30", "235", "-205", "1", "1"],
+        ["9.","Mahurangi College", "4", "0", "4", "0", "21", "250", "-229", "0", "0"],
+
+    ],
+};
+
+const months = [
+    "January", 
+    "February", 
+    "March", 
+    "April", 
+    "May", 
+    "June", 
+    "July", 
+    "August", 
+    "September", 
+    "October", 
+    "November", 
+    "December"
+];
+
+const Leagues = {
+    "rosmini1stxv": "North Harbour Rugby 1st XV Competition",
+}
