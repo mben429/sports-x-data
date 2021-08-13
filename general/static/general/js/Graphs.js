3b9776d49f999a2adebd050bc8a67de14d40ce66
Michael Bennett <mtbennett9010@gmail.com>
2021-08-13 17:29:37 +1200

    further progress on match centre.

--- general/static/general/js/Graphs.js
+++ general/static/general/js/Graphs.js
@@ -75,3 +75,315 @@ const MetersGainedGraph = () => {
         config
     );
 }
+
+const TackleSuccessPosGraph = () => {
+    const labels = [
+        'Backs',
+        'Forwards',
+        ];
+
+    const data = {
+        labels: labels,
+        datasets: [{
+            label: 'Tackle Success % by Position',
+            backgroundColor: massey_yellow,
+            borderColor: black_1,
+            barThickness: 75,
+            data: [66.66, 85],
+            }]
+        };
+
+    const config = {
+        type: 'bar',
+        data,
+        options: {
+            scales: {
+                x: {
+                    grid: {
+                        display: true,
+                        drawBorder: true,
+                        drawOnChartArea: false,
+                        drawTicks: true,
+                        color: "rgb(165,167,170)"
+                    },
+                    ticks: {
+                        font: {
+                            size: 20,
+                            family: "'Abel', sans-serif"
+                        },
+                        color: white,
+                        padding: 15
+                    }
+                },
+                y: {
+                    grid: {
+                        display: true,
+                        drawBorder: true,
+                        drawOnChartArea: false,
+                        drawTicks: true,
+                        color: "rgb(165,167,170)"
+                    },
+                    ticks: {
+                        font: {
+                            size: 16,
+                            family: "'Abel', sans-serif"
+                        },
+                        color: white,
+                        padding: 7
+                    },
+                    min: 0,
+                    max: 100,
+                    stepSize: 20,
+                }
+            },
+            plugins: {
+                legend: {
+                    display: false
+                }
+            },
+            responsive: true,
+            maintainAspectRatio: false
+        }
+        };
+
+
+    var myChart = new Chart(
+        document.getElementById('ts-pos-graph'),
+        config
+    );
+}
+
+
+const TackleSuccessByTypeGraph = () => {
+    const labels = [
+        'Above Waist',
+        'Below Waist',
+        ];
+
+    const data = {
+        labels: labels,
+        datasets: [{
+            label: 'Tackle Success % by Type of Tackle',
+            backgroundColor: massey_yellow,
+            borderColor: black_1,
+            barThickness: 75,
+            data: [75, 90],
+            }]
+        };
+
+    const config = {
+        type: 'bar',
+        data,
+        options: {
+            scales: {
+                x: {
+                    grid: {
+                        display: true,
+                        drawBorder: true,
+                        drawOnChartArea: false,
+                        drawTicks: true,
+                        color: "rgb(165,167,170)"
+                    },
+                    ticks: {
+                        font: {
+                            size: 20,
+                            family: "'Abel', sans-serif"
+                        },
+                        color: white,
+                        padding: 15
+                    }
+                },
+                y: {
+                    grid: {
+                        display: true,
+                        drawBorder: true,
+                        drawOnChartArea: false,
+                        drawTicks: true,
+                        color: "rgb(165,167,170)"
+                    },
+                    ticks: {
+                        font: {
+                            size: 16,
+                            family: "'Abel', sans-serif"
+                        },
+                        color: white,
+                        padding: 7
+                    },
+                    min: 0,
+                    max: 100,
+                    stepSize: 20,
+                }
+            },
+            plugins: {
+                legend: {
+                    display: false
+                }
+            },
+            responsive: true,
+            maintainAspectRatio: false
+        }
+        };
+
+
+    var myChart = new Chart(
+        document.getElementById('tck-aw-graph'),
+        config
+    );
+}
+
+const TackleSuccessByArea = () => {
+    const labels = [
+        'Own 5m',
+        'Opp 5m',
+        'Own 22',
+        'Opp 22',
+        'Midfield'
+        ];
+
+    const data = {
+        labels: labels,
+        datasets: [{
+            label: 'Tackle Success % by Area of Field',
+            backgroundColor: massey_yellow,
+            borderColor: black_1,
+            barThickness: 50,
+            data: [95, 75, 88, 79, 70],
+            }]
+        };
+
+    const config = {
+        type: 'bar',
+        data,
+        options: {
+            scales: {
+                x: {
+                    grid: {
+                        display: true,
+                        drawBorder: true,
+                        drawOnChartArea: false,
+                        drawTicks: true,
+                        color: "rgb(165,167,170)"
+                    },
+                    ticks: {
+                        font: {
+                            size: 20,
+                            family: "'Abel', sans-serif"
+                        },
+                        color: white,
+                        padding: 15
+                    }
+                },
+                y: {
+                    grid: {
+                        display: true,
+                        drawBorder: true,
+                        drawOnChartArea: false,
+                        drawTicks: true,
+                        color: "rgb(165,167,170)"
+                    },
+                    ticks: {
+                        font: {
+                            size: 16,
+                            family: "'Abel', sans-serif"
+                        },
+                        color: white,
+                        padding: 7
+                    },
+                    min: 0,
+                    max: 100,
+                    stepSize: 20,
+                }
+            },
+            plugins: {
+                legend: {
+                    display: false
+                }
+            },
+            responsive: true,
+            maintainAspectRatio: false
+        }
+        };
+
+
+    var myChart = new Chart(
+        document.getElementById('tck-area-graph'),
+        config
+    );
+}
+
+const LinebreaksByPos = () => {
+    const labels = [
+        "Backs",
+        "Forwards"
+        ];
+
+    const data = {
+        labels: labels,
+        datasets: [{
+            label: 'Linebreaks Allowed by Position',
+            backgroundColor: massey_yellow,
+            borderColor: black_1,
+            barThickness: 75,
+            data: [9, 5],
+            }]
+        };
+
+    const config = {
+        type: 'bar',
+        data,
+        options: {
+            scales: {
+                x: {
+                    grid: {
+                        display: true,
+                        drawBorder: true,
+                        drawOnChartArea: false,
+                        drawTicks: true,
+                        color: "rgb(165,167,170)"
+                    },
+                    ticks: {
+                        font: {
+                            size: 20,
+                            family: "'Abel', sans-serif"
+                        },
+                        color: white,
+                        padding: 15
+                    }
+                },
+                y: {
+                    grid: {
+                        display: true,
+                        drawBorder: true,
+                        drawOnChartArea: false,
+                        drawTicks: true,
+                        color: "rgb(165,167,170)"
+                    },
+                    ticks: {
+                        font: {
+                            size: 16,
+                            family: "'Abel', sans-serif"
+                        },
+                        color: white,
+                        padding: 7
+                    },
+                    min: 0,
+                    max: 15,
+                    stepSize: 5,
+                }
+            },
+            plugins: {
+                legend: {
+                    display: false
+                }
+            },
+            responsive: true,
+            maintainAspectRatio: false
+        }
+        };
+
+
+    var myChart = new Chart(
+        document.getElementById('lb-by-position-graph'),
+        config
+    );
+}
